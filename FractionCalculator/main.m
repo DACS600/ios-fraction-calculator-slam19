22257cc326fd2488c307d1aff0f3581043bf7af4
fatpandaboy <slam19@deerfield.edu>
2018-09-23 17:07:23 -0400

    Initial Commit

---
+++ FractionCalculator/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FractionCalculator
+//
+//  Created by Simon Lam on 9/23/18.
+//  Copyright © 2018 Simon Lam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
